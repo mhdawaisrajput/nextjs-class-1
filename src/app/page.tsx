8b9195544c1ae77127e1f678c59489c842e9884f
Muhammad Awais <162321104+mhdawaisrajput@users.noreply.github.com>
2024-11-02 08:46:26 -0700

    page.tsx code updated

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,31 +1,18 @@
 function HomePage(){
   return (
     <div>
-      <h1>Hello, from Awais.!</h1>
+      <h1>{`Hello, from Awais.!`}</h1>
     </div>
   )
 };
-
-function Age(){
-  let age = 18;
-  return (
-    <h1>I'm {age} years old.!</h1>
-  )
-};
-
 export default HomePage;
+
+// Method 02:
+
+// function Age(){
+//   let age = 18;
+//   return (
+//     <h1>I'm {age} years old.!</h1>
+//   )
+// };
 // export default Age;       // Just One function can be exported default at one time.
-
-//////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////.
-
-// Panaverse: step00_helloworld. 
-
-//  1...step00_helloworld/app/page.tsx
-function Home(){
-  return (
-    <div>Hello from Zia</div>
-  )
-};
-
-// 2...step00_helloworld/app/layout.tsx   === By_default.
-// 3...step00_helloworld/app/globals.css  === Empty.
